0f87d22317266a679872567d60bae3a9b6f605c0
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-08-02 10:21:50 +0200

    Setup files for PoC. Closes #7

---
+++ poc/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+print("""
+. . .,---.,---.,   .|,   .,---.|
+| | ||---||---'|\  |||\  ||  _.|
+| | ||   ||  \ | \ ||| \ ||   |
+`-'-'`   '`   ``  `'``  `'`---'o
+================================
+
+You are installing a proof-of-concept software that IS NOT PRODUCTION READY.
+
+""")
+
+setup()
