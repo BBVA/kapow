0c783447e35d2c7e16ad02296c13eae949ecb55a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-08-06 12:06:58 +0200

    Minimal poc in Go, executing one command and with two HTTP servers.

---
+++ gopoc/kapow.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	go func() {
+		fmt.Println("Listening on port 8080")
+		http.ListenAndServe(":8080", &userServerHandler{})
+	}()
+
+	http.ListenAndServe(":8081", &controlApiHandler{})
+}
+
+type userServerHandler struct {
+}
+
+func (m *userServerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("date").Output()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.Write(out)
+	}
+}
+
+type controlApiHandler struct {
+}
+
+func (m *controlApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the control API!"))
+}
