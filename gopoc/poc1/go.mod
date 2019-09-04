6affed99c8110303d6a4d8ee6f1c5ed7da70eb76
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-09-04 16:35:03 +0200

    Remove gopoc dir and make the main.go listen to 8080 and 8081 to further check the pipeline.

--- gopoc/poc1/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/BBVA/kapow
-
-go 1.12
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-    "fmt"
-
-    b "github.com/BBVA/kapow/pkg/banner"
+	"fmt"
+	b "github.com/BBVA/kapow/pkg/banner"
+	"net/http"
+	"os/exec"
 )
 
 func main() {
-    ban := b.Banner("0.1.0")
-    fmt.Println(ban)
+	ban := b.Banner("0.1.0")
+	fmt.Println(ban)
+
+	go func() {
+		http.ListenAndServe(":8080", &userServerHandler{})
+	}()
+	http.ListenAndServe(":8081", &controlAPIHandler{})
+
+}
+
+type userServerHandler struct {
+}
+
+func (m *userServerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("date").Output()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.Write(out)
+	}
+}
+
+type controlAPIHandler struct {
+}
+
+func (m *controlAPIHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the control API!"))
 }
