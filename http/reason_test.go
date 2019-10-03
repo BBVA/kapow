6689ed458c7cb357392a476ca82420533ea61ab1
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-10-03 14:06:04 +0200

    Better names for tests

--- http/reason_test.go
+++ http/reason_test.go
@@ -5,35 +5,35 @@ import (
 	"testing"
 )
 
-func TestEmptyStringIsEmptyReason(t *testing.T) {
+func TestEmptyReasonWhenEmptyString(t *testing.T) {
 	r := &nethttp.Response{Status: ""}
 	if GetReason(r) != "" {
 		t.Errorf("We consider an empty status line to have an empty reason")
 	}
 }
 
-func TestOnlyCodeIsEmptyReason(t *testing.T) {
+func TestEmptyReasonWhenOnlyCode(t *testing.T) {
 	r := &nethttp.Response{Status: "200"}
 	if GetReason(r) != "" {
 		t.Errorf("We consider an status line with just the status code to have an empty reason")
 	}
 }
 
-func TestOnlyCodePlusSpaceIsEmptyReason(t *testing.T) {
+func TestEmptyReasonWhenOnlyCodePlusSpace(t *testing.T) {
 	r := &nethttp.Response{Status: "200 "}
 	if GetReason(r) != "" {
 		t.Errorf("We consider an status line with just the status code to have an empty reason")
 	}
 }
 
-func TestOneWordReason(t *testing.T) {
+func TestReasonOfOneWord(t *testing.T) {
 	r := &nethttp.Response{Status: "200 FOO"}
 	if GetReason(r) != "FOO" {
 		t.Errorf("Unexpected reason found")
 	}
 }
 
-func TestMultiWordReason(t *testing.T) {
+func TestReasonOfMultipleWords(t *testing.T) {
 	r := &nethttp.Response{Status: "200 FOO BAR BAZ"}
 	if GetReason(r) != "FOO BAR BAZ" {
 		t.Errorf("Unexpected reason found")
