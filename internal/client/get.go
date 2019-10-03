6eb213a5455557999f8712f2a18d1a59964f1761
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-10-03 20:57:40 +0200

    Adjust set and get to send no Content-Type

--- internal/client/get.go
+++ internal/client/get.go
@@ -9,5 +9,5 @@ import (
 //GetData will perform the request and write the results on the provided writer
 func GetData(host, id, path string, wr io.Writer) error {
 	url := host + "/handlers/" + id + path
-	return http.Get(url, nil, wr)
+	return http.Get(url, "", nil, wr)
 }
--- internal/client/set.go
+++ internal/client/set.go
@@ -10,6 +10,6 @@ import (
 // TODO: Review spec: Data API > Error responses > 204 Resource Item Not Found should not be 2xx
 func SetData(kapowURL, handlerId, path string, r io.Reader) error {
 
-	return http.Put(fmt.Sprintf("%s/%s%s", kapowURL, handlerId, path), r, nil)
+	return http.Put(fmt.Sprintf("%s/%s%s", kapowURL, handlerId, path), "", r, nil)
 
 }
