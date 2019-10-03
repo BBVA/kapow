df96d322fcb0a30872f7743c5ee31ddeea536b57
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-10-03 22:47:43 +0200

    Don't set Content-Type to application/json in query without body

--- internal/client/route_list.go
+++ internal/client/route_list.go
@@ -9,5 +9,5 @@ import (
 // ListRoutes list the routes registered on the kapow! instance
 func ListRoutes(host string, w io.Writer) error {
 	url := host + "/routes/"
-	return http.Get(url, "application/json", nil, w)
+	return http.Get(url, "", nil, w)
 }
