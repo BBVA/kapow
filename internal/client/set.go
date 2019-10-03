ffa262b2b6bbbe5b60d1c9e5b6dfcc98e32e32df
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-10-03 16:49:12 +0200

    Simplify internal/client/set.go by leveraging internal/http.Request()
    Co-authored-by: Héctor Hurtado <hector.hurtado@bbva.com>

--- internal/client/set.go
+++ internal/client/set.go
@@ -3,34 +3,13 @@ package client
 import (
 	"fmt"
 	"io"
-	"net/http"
+
+	"github.com/BBVA/kapow/internal/http"
 )
 
-const (
-	errMandatoryParam   = "Mandatory parameter %s missing"
-	errInvalidURL       = "kapowURL, handlerId or path has invalid format"
-	errNotFound         = "Resource Item Not Found"
-	errNotValidResource = "Invalid Resource Path"
-	serverURLTemplate   = "%s/%s%s"
-)
-
+// TODO: Review spec: Data API > Error responses > 204 Resource Item Not Found should not be 2xx
 func SetData(kapowURL, handlerId, path string, r io.Reader) error {
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf(serverURLTemplate, kapowURL, handlerId, path), r)
-	if err != nil {
-		return err
-	}
+	return http.Put(fmt.Sprintf("%s/%s%s", kapowURL, handlerId, path), r, nil)
 
-	kpowClient := &http.Client{}
-	if resp, err := kpowClient.Do(req); err != nil {
-		return err
-	} else if resp.StatusCode == http.StatusNoContent {
-		return fmt.Errorf(errNotFound)
-	} else if resp.StatusCode == http.StatusBadRequest {
-		return fmt.Errorf(errNotValidResource)
-	} else if resp.StatusCode >= http.StatusNotFound {
-		return fmt.Errorf(resp.Status[4:])
-	}
-
-	return nil
 }
