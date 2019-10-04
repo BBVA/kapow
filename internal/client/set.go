a865ade26a12f2271fa5796b45fd7f13b684d198
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-10-04 07:28:08 +0200

    Use string concatenation to compose simple expression
    Co-authored-by: Héctor Hurtado <hector.hurtado@bbva.com>

--- internal/client/set.go
+++ internal/client/set.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/BBVA/kapow/internal/http"
@@ -9,7 +8,5 @@ import (
 
 // TODO: Review spec: Data API > Error responses > 204 Resource Item Not Found should not be 2xx
 func SetData(kapowURL, handlerId, path string, r io.Reader) error {
-
-	return http.Put(fmt.Sprintf("%s/%s%s", kapowURL, handlerId, path), "", r, nil)
-
+	return http.Put(kapowURL+"/"+handlerId+path, "", r, nil)
 }
