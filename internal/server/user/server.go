10586bae95c1c825578fd26bb95fa3c159ea75ec
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-10-07 19:05:59 +0200

    Add internal/server/user/server{,_test}.go
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

---
+++ internal/server/user/server.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+type swappableMux struct {
+	m    sync.RWMutex
+	root mux.Router
+}
+
+func (sm *swappableMux) get() mux.Router {
+	sm.m.RLock()
+	defer sm.m.RUnlock()
+
+	return sm.root
+}
+
+func (sm *swappableMux) set(mux mux.Router) {
+	sm.m.Lock()
+	sm.root = mux
+	sm.m.Unlock()
+}
+
+var Server http.Server
+
+func Run() {
+}
