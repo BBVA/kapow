9e767a866c2cdbc7839149ff284f1acade90b311
Héctor Hurtado <hector.hurtado@bbva.com>
2019-10-12 22:08:14 +0200

    Added simple resource functions

--- internal/server/data/server.go
+++ internal/server/data/server.go
@@ -1,1 +1,22 @@
 package data
+
+import (
+	"errors"
+	"net/http"
+)
+
+func getRequestMethod(req *http.Request) (string, error) { return req.Method, nil }
+
+func getRequestHost(req *http.Request) (string, error) { return req.Host, nil }
+
+func getRequestPath(req *http.Request) (string, error) { return req.URL.EscapedPath(), nil }
+
+func setResponseStatus(res http.ResponseWriter, value interface{}) error {
+
+	if val, ok := value.(int); ok {
+		res.WriteHeader(val)
+		return nil
+	}
+
+	return errors.New("Not an integer value")
+}
--- internal/server/data/server_test.go
+++ internal/server/data/server_test.go
@@ -1,1 +1,70 @@
 package data
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetRequestMethodReturnsCorrectValue(t *testing.T) {
+	req := httptest.NewRequest("GET", "/this/is/a/test?with=params&that=works", nil)
+
+	req.Host = "www.example.com"
+	value, err := getRequestMethod(req)
+	if err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if value != "GET" {
+		t.Errorf("Unexpected value. Expected: %s, got: %s", "GET", value)
+	}
+}
+
+func TestGetRequestHostReturnsCorrectValue(t *testing.T) {
+	req := httptest.NewRequest("GET", "/this/is/a/test?with=params&that=works", nil)
+
+	req.Host = "www.example.com"
+	value, err := getRequestHost(req)
+	if err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if value != "www.example.com" {
+		t.Errorf("Unexpected value. Expected: %s, got: %s", "www.example.com", value)
+	}
+}
+
+func TestGetRequestPathReturnsCorrectValue(t *testing.T) {
+	req := httptest.NewRequest("GET", "/this/is/a/test?with=params&that=works", nil)
+
+	req.Host = "www.example.com"
+	value, err := getRequestPath(req)
+	if err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if value != "/this/is/a/test" {
+		t.Errorf("Unexpected value. Expected: %s, got: %s", "/this/is/a/test", value)
+	}
+}
+
+func TestSetResponseStatusSetsCorrectValue(t *testing.T) {
+	res := httptest.NewRecorder()
+
+	err := setResponseStatus(res, 500)
+	if err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if res.Result().StatusCode != 500 {
+		t.Errorf("Unexpected value. Expected: %d, got: %d", 500, res.Result().StatusCode)
+	}
+}
+
+func TestSetResponseStatusFailsWhenNotInteger(t *testing.T) {
+	res := httptest.NewRecorder()
+
+	err := setResponseStatus(res, "200")
+	if err == nil {
+		t.Errorf("Expecting an error but got OK")
+	}
+}
