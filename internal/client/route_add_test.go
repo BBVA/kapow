52ae3375a4c7dd54900a883a572e3af564baa5b5
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-10-23 18:52:20 +0200

    Drop stale TODO comment
    Close #42

--- internal/client/route_add_test.go
+++ internal/client/route_add_test.go
@@ -36,11 +36,6 @@ func TestSuccessOnCorrectRoute(t *testing.T) {
 		Reply(http.StatusCreated).
 		JSON(map[string]string{})
 
-	// TODO: As per the spec¹, the call should return a JSON body with the info
-	// of the newly created route.  Should we consider this in the mocked server
-	// and in the test?
-	// ¹: https://github.com/BBVA/kapow/tree/master/spec#insert-a-route
-
 	err := AddRoute(
 		"http://localhost",
 		"/hello", "GET", "", "echo Hello World | kapow set /response/body", nil)
