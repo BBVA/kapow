2d5743af843ec714b735471737fe50ea6b5c07ee
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2019-11-13 18:18:44 +0100

    Add blank line between legal header and package declaration
    Otherwise, the legal header is rendered as package documentation.

--- internal/server/model/route.go
+++ internal/server/model/route.go
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package model
 
 // Route contains the data needed to represent a Kapow! user route.
