91aff009b0ca4157910ebc5f4a3bbc8b0d8e8ef9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-12-16 11:59:36 +0100

    doc: Remove custom latex rst parts from exclude to avoid compilation problems

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['latextoc.rst', '*/toc.rst']
+exclude_patterns = []
 
 rst_prolog = """
 .. role:: tech(code)
